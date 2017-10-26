30bcff97a8ba5139fe2b5a550439463af25b38b0
Tommy <276386551@qq.com>
2017-10-26 18:10:56 +0800

    初始提交

---
+++ src/main/java/com/bit/api/core/ApiRequest.java
@@ -0,0 +1,71 @@
+package com.bit.api.core;
+
+/**
+ * Created by Tommy on 2017/10/26.
+ */
+public class ApiRequest {
+
+    private String memberId;
+    private String accessToken;
+    private String uCode;
+    private String eCode;
+    private String timestamp;
+    private String clientIp;
+    private String uuId;
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getuCode() {
+        return uCode;
+    }
+
+    public void setuCode(String uCode) {
+        this.uCode = uCode;
+    }
+
+    public String geteCode() {
+        return eCode;
+    }
+
+    public void seteCode(String eCode) {
+        this.eCode = eCode;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getClientIp() {
+        return clientIp;
+    }
+
+    public void setClientIp(String clientIp) {
+        this.clientIp = clientIp;
+    }
+
+    public String getUuId() {
+        return uuId;
+    }
+
+    public void setUuId(String uuId) {
+        this.uuId = uuId;
+    }
+}
